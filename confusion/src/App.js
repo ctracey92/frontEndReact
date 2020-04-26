4ed16386b2d862c5d1bb54c6e01b62f506d3880b
Ctracey92 <cody.tracey92@gmail.com>
2020-04-25 20:09:24 -0500

    added in first component and modified the app.js page to utilize it

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import {Navbar, NavbarBrand, Media} from 'reactstrap';
+import Menu from "./components/MenuComponent";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color='primary'>
-          <div className="conatiner">
+          <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
-        </Navbar>
+        </Navbar> 
+        <Menu />
       </div>
     );
   }
