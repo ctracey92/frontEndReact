13e8e52584f2de7d355426d2c51cc67f10fd82d8
Ctracey92 <cody.tracey92@gmail.com>
2020-05-05 20:46:32 -0500

    added in the home page sub components and data needed

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
 import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
-import { DISHES } from "../shared/dishes";
 import Contact from "./ContactComponent";
 import Header from "./Header";
 import Footer from "./Footer";
-import { Switch, Route, Redirect } from "react-router-dom";
+
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 
 class MainComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          promotion={
+            this.state.promotions.filter((promotion) => promotion.featured)[0]
+          }
+        />
+      );
     };
     return (
       <div>
